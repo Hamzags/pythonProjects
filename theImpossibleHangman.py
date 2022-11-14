33e7507c0a6148c5889f80f01a9d5482c88d008a
Hamzags <84027983+Hamzags@users.noreply.github.com>
2022-11-14 18:12:23 -0500

    Update and rename hangmanGame.py to theImpossibleHangman.py

--- theImpossibleHangman.py
+++ theImpossibleHangman.py
@@ -1,7 +1,7 @@
-#importing random words module in order to generate a random word for user to guess
+#importing random words module in order to generate a random word for player to guess
 from random_word import RandomWords
 
-#generate word and split letters into list
+#generate word and split letters into list in order to compare player guesses with the word and gradually show correctly guessed letters.
 generateRandomWord = RandomWords()
 hangmanValue = generateRandomWord.get_random_word()
 hangmanWordListed = list(hangmanValue)
@@ -10,20 +10,20 @@ hangmanGuessList = []
 for i in range(0, wordLength):
     hangmanGuessList.append('_')
 
-#initializing the guessCount variable as user has 10 chances before game is over.
+#initializing the guessCount variable as player has 10 chances before game is over.
 guessCount=0
 
-#start guessing
-print("The hangman game has begun! The word to guess contains", wordLength, "letters!")
-#print(hangmanWordListed)
+#displaying message letting player know the game has started as well as the number of letters in the word to guess
+print("Starting the impossible Hangman! The word to guess contains", wordLength, "letters!")
 print(hangmanGuessList)
 
 
-#Main while loop that runs the game while the variable below is equals to true.
+#Main while loop that runs the game while the variable isGameActive is True or as long as the list of letters guessed by player is not equals to the list of letters in the generated word
 isGameActive = True
 while isGameActive == True or hangmanGuessList != hangmanWordListed:
-    isInHangmanAtLeastOnce = 1
 
+
+#adding constraints to user input : letters only & only one letter can be guessed at a time. Also using the lower() method to prevent any errors related to case sensitivity
     userGuess = input("Start Guessing: ").lower()
     while len(userGuess) > 1:
         print("You can only guess one letter at time!")
@@ -32,9 +32,10 @@ while isGameActive == True or hangmanGuessList != hangmanWordListed:
         print("Only letters are accepted!")
         userGuess = input("Start Guessing: ").lower()
 
-    incorrectGuessIndex = None
-    incorrectGuess = 0
-    guessesLeft = 10 - guessCount
+    # managing occurences of player guessed letters in the list of letters in the generated word and initializing variables for occurence of guessed
+    # letter in the generated word and inserting the letter if guessed  correctly in our created list at the same index it is in the list of letters for the generated word
+    isInHangmanAtLeastOnce = 1
+    guessesLeft = 9 - guessCount
 
     if isInHangmanAtLeastOnce == hangmanWordListed.count(userGuess):
         correctGuessIndex = hangmanWordListed.index(userGuess)
@@ -51,26 +52,32 @@ while isGameActive == True or hangmanGuessList != hangmanWordListed:
         guessCount += 1
         print("Wrong guess! You have", guessesLeft, "tries left.")
 
-    #User guessed word correctly
+    #managing the message displayed to the player in case of a player win and offering choice to start a new game.
     if hangmanWordListed == hangmanGuessList:
         print("Congratulations! You beat the impossible hangman game!")
         isGameActive = False
         startNewGame = input("Start a new game? Y/N").lower()
 
+    # managing the message displayed to the player in case of a player loss and offering choice to start a new game.
+    elif guessesLeft == 0:
+        print("Game Over. Better luck next time!")
+        isGameActive = False
+        startNewGame = input("Start a new game? Y/N").lower()
+
+    #managing events that happen if user chooses to either a start a new game or not and displaying messages accordingly.
         if startNewGame != "y":
             isGameActive = False
             print("Thanks for playing!")
+
+
         else:
             generateRandomWord = RandomWords()
             hangmanValue = generateRandomWord.get_random_word()
             hangmanWordListed = list(hangmanValue)
             wordLength = len(hangmanWordListed)
             hangmanGuessList = []
-
             for i in range(0, wordLength):
                 hangmanGuessList.append('_')
-
-            print("The hangman game has begun! The word to guess contains", wordLength, "letters!")
-            print(hangmanWordListed)
+            print("Starting the impossible Hangman! The word to guess contains", wordLength, "letters!")
             print(hangmanGuessList)
             isGameActive = True
